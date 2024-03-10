a5ac21053c63941b4f8009c7147aac771372f409
Alexander Rafferty <websites@alexanderrafferty.com>
2024-03-10 21:26:48 +1100

    Remove home

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
 import { Route, Router } from '@solidjs/router'
-import HomePage from './pages/Home'
 import BoardPage from './pages/Board'
 import PlayerPage from './pages/Player'
 import ConsolePage from './pages/Console'
@@ -10,7 +9,6 @@ import './index.css'
 render(
   () => (
     <Router base={import.meta.env.BASE_URL}>
-      <Route path="/" component={HomePage} />
       <Route path="/board" component={BoardPage} />
       <Route path="/player" component={PlayerPage} />
       <Route path="/console" component={ConsolePage} />
